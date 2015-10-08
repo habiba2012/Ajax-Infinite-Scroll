60356cce419b73f8fc66938bd36e49b9a03c63c4
wayneho <wayneho92@hotmail.com>
2015-10-07 19:05:34 -0700

    add layout of movie content

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+var movieList = [];
 
+var MoviePrototype = {
+	init: function(id){
+		this.id = id;
+	},
+	get_id: function(){
+		return this.id;
+	}
+}
 
 $(document).ready(function(){
 	$('footer').hide();
@@ -13,6 +22,13 @@ $(document).ready(function(){
 		});
 	});
 
+	$('#movie-list').delegate('.movie-content img', 'click', function(event){
+		$(this).toggleClass('reduce-size');
+		var movie_index = $(event.target).closest('li').index('li');
+		var $el = $(event.target).closest('.movie-content');
+		console.log(movie_index);
+		get_movie_content(movie_index, $el);
+	});
 })
 .ajaxStart(function() {
 	$('footer').show();
@@ -22,41 +38,51 @@ $(document).ready(function(){
 });
 
 
+var create_poster = function(movie){
+	var img = movie.Poster?movie.Poster:"";
+	var newMovie = Object.create(MoviePrototype);
+	newMovie.init(movie.imdbID);
+	movieList.push(newMovie);
+	return $('<li style="display: inline-block"><div><p title="' + movie.Title + '">' + movie.Title + '</p><div class="movie-content"><img src='+ img + '></div></div></li>').fadeIn();
+}
 
-var create_poster = function(title, img){
-	return $('<li style="display: inline-block"><p title="' + title + '">' + title + '</p><img src='+ img + '></li>').fadeIn();
+// p: the search parameter(i,t,s)
+// val: the value of the search parameter
+// onSuccess: the function to call when a movie is retrieved
+var ajax_call = function(p, val, onSuccess){
+	var data = {type: 'movie'};
+	data[p] = val;
+	$.ajax({
+		type: 'GET',
+		url: 'http://www.omdbapi.com/?',
+		data,
+		success: function(movies){
+			console.log(movies);
+			onSuccess(movies);
+		},
+		error: function( xhr, status, errorThrown ) {
+	        alert( "Sorry, there was a problem!" );
+	        console.log( "Error: " + errorThrown );
+	        console.log( "Status: " + status );
+	        console.dir( xhr );
+	    }
+	});
 }
 
 var search_for_movie = function(){
 	var search = $('#search-field');
+	var onSuccess = function(movies){
+		for(var i = 0; i < 9; i++){
+			if(typeof movies.Search === 'undefined')
+				break;
+			$('#movie-list').append(create_poster(movies.Search[i]));
+		}
+	}
 	$('.movie-form').submit(function(event){
 		if( search.val().length > 0){	
 			clear_list();
 			$(window).unbind('scroll');
-			$.ajax({
-				type: 'GET',
-				url: 'http://www.omdbapi.com/?',
-				data: {
-					s: search.val(),
-					type: 'movie',
-				},
-				success: function(movies){
-					console.log(movies);
-					for(var i = 0; i < 9; i++){
-						if(typeof movies.Search === 'undefined')
-							break;
-						var img = movies.Search[i].Poster?movies.Search[i].Poster:"";
-						var title = movies.Search[i].Title
-						$('#movie-list').append(create_poster(title,img));
-					}
-				},
-				error: function( xhr, status, errorThrown ) {
-			        alert( "Sorry, there was a problem!" );
-			        console.log( "Error: " + errorThrown );
-			        console.log( "Status: " + status );
-			        console.dir( xhr );
-			    }
-			});
+			ajax_call('s', search.val(), onSuccess);
 			search.val("");
 			event.preventDefault();
 		}
@@ -68,35 +94,30 @@ var get_random_movie = function(){
 	var pad = "0000000";
 	var id = "tt"+pad.substring(0,pad.length-rand.length)+rand;
 	console.log(id);
-	$.ajax({
-		type: 'GET',
-		url: 'http://www.omdbapi.com/?',
-		data: {
-			i: id,
-			type: 'movie'
-		},
-		success: function(movie){
-			console.log(movie);
 
-			if(typeof movie.Title !== 'undefined'){
-				var img = movie.Poster?movie.Poster:"";
-				var title = movie.Title
-				$('#movie-list').append(create_poster(title,img));
-				$(window).scrollTop($(window).scrollTop());
-			}else{
-				get_random_movie();
-			}
-			
-		},
-		error: function( xhr, status, errorThrown ) {
-	        alert( "Sorry, there was a problem!" );
-	        console.log( "Error: " + errorThrown );
-	        console.log( "Status: " + status );
-	        console.dir( xhr );
-	    }
-	});
-
+	var onSuccess = function(movie){
+		if(typeof movie.Title !== 'undefined'){
+			$('#movie-list').append(create_poster(movie));
+			$(window).scrollTop($(window).scrollTop());
+		}else{
+			get_random_movie();
+		}
+	}
+	ajax_call('i', id, onSuccess);
 };
+
+var get_movie_content = function(index, el){
+	var id = get_imbdID(index);
+	var onSuccess = function(){
+		el.append($('<div class="rating" style="display: inline-block"></div><div class="synopsis"></div>').fadeIn());
+	}
+	ajax_call('i', id, onSuccess);
+}
+
+
+var get_imbdID = function(index){
+	return(movieList[index].get_id());
+}
 
 var get_num_of_movies = function(num){
 	for(var i = 0; i < num; i++){
@@ -110,6 +131,8 @@ var clear_list = function(){
 	    myNode.removeChild(myNode.firstChild);
 	}
 };
+
+
 
 
 var opts = {
